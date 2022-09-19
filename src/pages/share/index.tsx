ee6a4525e6faef43fba58ad1f04bddda1cbafe9d
cliexec <cli.exec.com@gmail.com>
2022-09-19 10:52:12 +0700

    add page title

--- src/pages/share/index.tsx
+++ src/pages/share/index.tsx
@@ -1,6 +1,8 @@
 import * as React from "react"
 import Head from "next/head"
 import Box from "@mui/material/Box"
+import Divider from "@mui/material/Divider"
+import Typography from '@mui/material/Typography';
 import DashboardLayout from "../../components/DashboardLayout"
 import RejectionForm from "../../features/rejection/RejectionForm"
 import { useRetrieveActivityQuery } from "../../features/activity/Api"
@@ -26,6 +28,8 @@ const Share = (props: any) => {
 						md: 4,
 					}}
 				>
+					<Typography variant="h5" marginBottom={.5}>{"Share Rejection"}</Typography>
+					<Divider sx={{ marginBottom: 4 }} />
 					<RejectionForm id={id} action={action} activity={!isLoading && isSuccess ? data?.[0] : {}} isLoading={isLoading} />
 				</Box>
 			</DashboardLayout>
